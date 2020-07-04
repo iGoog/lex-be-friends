ac98c932005541b7231047d6290e110316e8d936
iGoog <paul@picayune.co>
2020-07-03 18:57:01 -0700

    display those words...

--- src/components/LexFriends/Words.vue
+++ src/components/LexFriends/Words.vue
@@ -1,12 +1,38 @@
 <template>
     <div class="wordsArea">
+            <div v-for="(word, i ) in playedWords" :key="i">
+                <ul class="wordRow">
+                    <li v-for="card in word.cards" :key="card.id">
+                        <Card :="card"/>
+                    </li>
+                    <li :key="`edit${i}`" @click="clickEdit(i)">
+                        <CardButton :="{content: '✍'}" />
+                    </li>
+                </ul>
+            </div>
 
     </div>
 </template>
 
 <script>
+	import {useStore} from "vuex";
+	import {reactive, ref, watch} from "vue";
+	import fakeTouchDragFactory from "../../util/fakeTouchDragFactory";
+	import Card from "./Card.vue";
+	import CardButton from "./CardButton.vue";
+
 	export default {
-		name: "Words"
+		name: "Words",
+		components: {CardButton, Card},
+		setup() {
+			const store = useStore();
+			const playedWords = reactive(store.state.lexGame.game.playField);
+			const clickEdit = (editIndex) => {
+
+            }
+
+			return { playedWords };
+        }
 	}
 </script>
 
@@ -15,5 +41,20 @@
     width: 100%;
     height: 100%;
     background-color: blanchedalmond;
+}
+.wordRow {
+    display: inline-flex;
+    justify-content: center;
+    align-content: center;
+    list-style: none;
+    padding: 0px;
+    padding-inline-start: 0px;
+}
+.wordRow li {
+    width: 4vmin;
+    height: 4vmin;
+    margin: 0.2vmin;
+    padding: 0;
+
 }
 </style>
