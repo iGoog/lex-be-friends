718415ba1ed3cf2004dceca5c85161ffff43a8b6
iGoog <paul@picayune.co>
2020-07-14 14:41:36 -0700

    animated Popovers :D

---
+++ src/components/popover/Popover.vue
@@ -0,0 +1,71 @@
+<template>
+<transition appear name="slide">
+<div v-if="show" :class="{wide}" class="popoverModal">
+    <slot></slot>
+</div>
+</transition>
+</template>
+
+<script>
+	export default {
+		name: "Popover",
+        props: {
+	        show: Boolean,
+            wide: Boolean
+        }
+	}
+</script>
+
+<style scoped>
+.popoverModal {
+    background: linear-gradient(0deg,#00EEEE 0%, #F0F0F0 100%);
+    padding: 2vh 2vw;
+    position:absolute;
+    opacity: 100%;
+    z-index: 900;
+    border-radius: 2vw;
+
+    top: 10vh;
+    min-height: 25vh;
+    max-height: 70vh;
+    box-shadow: 0 8px 10px -5px rgba(0,0,0,.2),0 16px 24px 2px rgba(0,0,0,.14),0 6px 30px 5px rgba(0,0,0,.12);
+
+    left: 28vw;
+    width: 40vw;
+}
+.slide-enter-active, .slide-leave-active {
+    transition: opacity 1.2s cubic-bezier(0.250, 0.460, 0.450, 0.940),
+                transform 1.2s cubic-bezier(0.250, 0.460, 0.450, 0.940),
+                transform-origin 1.2s cubic-bezier(0.250, 0.460, 0.450, 0.940);
+}
+.slide-enter-from {
+    transform: translateY(-600px) rotateX(30deg) scale(6.5);
+    transform-origin: 50% 200%;
+    opacity: 0;
+}
+.slide-enter-to {
+    transform: translateY(0) rotateX(0) scale(1);
+    transform-origin: 50% -500px;
+    /*transform-origin: 50% -500px;*/
+    opacity: 1;
+}
+
+.slide-leave-from {
+    transform: translateY(0) rotateX(0) scale(1);
+    transform-origin: 50% 1400px;
+    opacity: 1;
+}
+
+.slide-leave-to {
+    transform: translateY(-600px) rotateX(-30deg) scale(0);
+    transform-origin: 50% 100%;
+    opacity: 1;
+}
+
+
+.popoverModal.wide {
+    left: 8vw;
+    width: 80vw;
+}
+
+</style>
---
+++ src/components/popover/PopoverShade.vue
@@ -0,0 +1,39 @@
+<template>
+    <transition appear name="fade">
+        <div v-show="show"  class="popoverShade"></div>
+    </transition>
+
+</template>
+
+<script>
+	export default {
+		name: "PopoverShade",
+        props: {
+			show: Boolean
+        }
+	}
+</script>
+
+<style scoped>
+.popoverShade {
+    background: linear-gradient(0deg,#000000 0%, #111111 100%);
+    opacity: 85%;
+    position: absolute;
+    /* body margin: 5vh 0px 7vh 0px*/
+    width: 100%;
+    height: 88vh;
+    z-index: 100;
+    padding: 0;
+    margin: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+    transition: opacity 1.2s;
+}
+.fade-leave-to, .fade-enter-from {
+    opacity: 0;
+}
+.fade-leave-from, .fade-enter-to {
+    opacity: 85%;
+}
+</style>
