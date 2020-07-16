9c97cddce4b53dd8d66f6a508fc584aaf0e48d91
iGoog <paul@picayune.co>
2020-07-15 18:51:50 -0700

    pulsing animation starter

--- src/components/LexFriends/Play.vue
+++ src/components/LexFriends/Play.vue
@@ -4,6 +4,7 @@
          v-on:dragover.prevent="dragHover($event)"
          v-on:dragenter.prevent
     >
+        <div class="glow"><span class="fakery"></span></div>
         <transition-group class="cardRow" name="fade" tag="ul" >
             <li v-for="card in playCards" :id="card.id" :key="card.id"
                 :class="{
@@ -39,11 +40,59 @@
 
 <style scoped>
 
+.cardRow {
+
+}
 
 .playArea {
-    /*background: rgb(160,255,248);*/
-    /*background: linear-gradient(126deg, rgba(160,255,248,0.19091386554621848) 4%, rgba(153,193,224,0.20211834733893552) 48%, rgba(0,141,170,0.21052170868347342) 100%);*/
 
+}
 
+.glow {
+    width: 98vw;
+    height: 10vmin;
+    border-radius: 2vmin;
+    z-index: -1;
+    position: absolute;
+    overflow: hidden;
+    text-align: center;
+}
+
+.fakery {
+    background-color: blue;
+    width: 30vmin;
+    height: 30vmin;
+    margin-top: -10vmin; /* (30-10)/2 */
+    border-radius: 2vmin;
+    background: radial-gradient(circle,
+    rgba(227,51,128,1) 0%,
+    rgba(227,51,128,1) 4.6%,
+    rgba(120,98,209,1) 11.6%,
+    rgba(148,187,233,1) 20.0%,
+    rgba(148,187,233,0) 26.0%
+    );
+    /*background: radial-gradient(circle, blue 0%, red 100%);*/
+    opacity: 50%;
+    background-size: 15vw 15vw;
+    background-position: center center;
+    background-repeat: no-repeat;
+    z-index: -10;
+    display: inline-block;
+    animation: glow 5s linear infinite both;
+}
+
+@keyframes glow {
+    0% {
+        transform: scale(0.2);
+        opacity: 50%;
+    }
+    50% {
+        transform: scale(25);
+        opacity: 30%;
+    }
+    100% {
+        transform: scale(10);
+        opacity: 0;
+    }
 }
 </style>
